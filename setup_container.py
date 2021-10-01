00f423c3b3df41aab44fa3a7ff77c2b575e93ae1
Łukasz Piłatowski <lukpila29@gmail.com>
2021-10-01 11:31:07 +0200

    add legacy versions with python 3.7

--- setup_container.py
+++ setup_container.py
@@ -9,6 +9,9 @@ versions = {
     "2021_2": Path("/opt/intel/openvino2021_2"),
     "2021_1": Path("/opt/intel/openvino2021_1"),
     "2020_4": Path("/opt/intel/openvino2020_4"),
+}
+
+legacy = {
     "2020_3": Path("/opt/intel/openvino2020_3"),
     "2020_2": Path("/opt/intel/openvino2020_2"),
     "2020_1": Path("/opt/intel/openvino2020_1"),
@@ -22,7 +25,7 @@ def abs_str(path: Path):
     return str(path.absolute())
 
 
-def create_venv(name: str, path: Path):
+def create_venv(name: str, path: Path, interpreter):
     req_path = path / "deployment_tools" / "model_optimizer" / "requirements.txt"
     venv_path = Path("/app") / "venvs" / ("venv"+name)
     venv_python_path = venv_path / "bin" / "python"
@@ -32,7 +35,7 @@ def create_venv(name: str, path: Path):
         del new_env["PYTHONHOME"]
     new_env["VIRTUAL_ENV"] = abs_str(venv_path)
     new_env["PATH"] = abs_str(venv_path / "bin") + ":" + new_env["PATH"]
-    subprocess.check_call([sys.executable, "-m", "venv", abs_str(venv_path)])
+    subprocess.check_call([interpreter, "-m", "venv", abs_str(venv_path)])
     subprocess.check_call([abs_str(venv_python_path), "-m", "pip", "install", "-U", "pip"], env=new_env)
     subprocess.check_call([abs_str(venv_python_path), "-m", "pip", "install", "-r", abs_str(req_path)], env=new_env)
     subprocess.check_call([abs_str(venv_python_path), "-m", "pip", "install", *additional_packages], env=new_env)
@@ -40,5 +43,7 @@ def create_venv(name: str, path: Path):
 
 if __name__ == "__main__":
     for env_name, base_path in versions.items():
-        create_venv(env_name, base_path)
+        create_venv(env_name, base_path, "python3.8")
+    for env_name, base_path in legacy.items():
+        create_venv(env_name, base_path, "python3.7")
 
