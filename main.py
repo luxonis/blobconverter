6c04ec189d196b5a5cadfe5934432f59475fb0a2
Łukasz Piłatowski <lukpila29@gmail.com>
2021-10-10 17:37:48 +0200

    update cert names

--- main.py
+++ main.py
@@ -373,8 +373,8 @@ def root():
 
 
 if Path('/app/ssl').is_dir():
-    cert = Path('/app/ssl/fullchain.pem')
-    key = Path('/app/ssl/privkey.pem')
+    cert = Path('/app/ssl/cert.pem')
+    key = Path('/app/ssl/server.key')
     ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     ctx.load_cert_chain(cert, key)
     app.run(host='0.0.0.0', port=8080, ssl_context=ctx)
