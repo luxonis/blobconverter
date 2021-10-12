74b2a5ab7e730e9ad0a2bdb0b5b6391965ef4cda
Łukasz Piłatowski <lukpila29@gmail.com>
2021-10-12 10:10:42 +0200

    make a better check for cert and key

--- main.py
+++ main.py
@@ -371,10 +371,10 @@ def get_zoo_models():
 def root():
     return app.send_static_file('index.html')
 
+cert = Path('/app/ssl/cert.pem')
+key = Path('/app/ssl/server.key')
 
-if Path('/app/ssl').is_dir():
-    cert = Path('/app/ssl/cert.pem')
-    key = Path('/app/ssl/server.key')
+if cert.exists() and key.exists():
     ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     ctx.load_cert_chain(cert, key)
     app.run(host='0.0.0.0', port=8080, ssl_context=ctx)
