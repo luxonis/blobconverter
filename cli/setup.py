2f5171c803ee69f10077c4c0d4f99eccb5dc69dc
Łukasz Piłatowski <lukpila29@gmail.com>
2021-10-13 15:24:22 +0200

    fix typo

--- cli/setup.py
+++ cli/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='blobconverter',
     version='1.2.3',
-    description='The tool that allows you to covert neural networks to MyriadX blob',
+    description='The tool that allows you to convert neural networks to MyriadX blob',
     long_description=io.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url='https://github.com/luxonis/blobconverter',
