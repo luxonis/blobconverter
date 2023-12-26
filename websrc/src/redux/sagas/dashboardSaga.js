2261e3c54a8c792c902106b857e626160fdb9fac
Jan Čuhel <jcuhel98@gmail.com>
2023-12-26 22:31:15 +0100

    Update error msg

--- websrc/src/redux/sagas/dashboardSaga.js
+++ websrc/src/redux/sagas/dashboardSaga.js
@@ -87,7 +87,6 @@ function* convertModel({payload}) {
           framework = "caffe";
           // Check for valid name
           valid_names = isValidName(payload['caffe-model'].name) && isValidName(payload['caffe-proto'].name)
-          console.log("FILENAMES: ", payload['caffe-model'].name, payload['caffe-proto'].name,  valid_names)
           optimizer_additional = ` --input_model=$dl_dir/${precision}/${payload['caffe-model'].name} --input_proto=$dl_dir/${precision}/${payload['caffe-proto'].name}`
           break;
         }
@@ -95,14 +94,12 @@ function* convertModel({payload}) {
           framework = "dldt";
           // Check for valid name
           valid_names = isValidName(payload['openvino-bin'].name) && isValidName(payload['openvino-xml'].name)
-          console.log("FILENAMES: ", payload['openvino-bin'].name, payload['openvino-xml'].name, valid_names)
           break;
         }
         case 'tf': {
           framework = "tf";
           // Check for valid name
           valid_names = isValidName(payload['tf-model'].name)
-          console.log("FILENAME: ", payload['tf-model'].name, valid_names)
           optimizer_additional = ` --input_model=$dl_dir/${precision}/${payload['tf-model'].name}`
           break;
         }
@@ -110,13 +107,12 @@ function* convertModel({payload}) {
           framework = "onnx";
           // Check for valid name
           valid_names = isValidName(payload['onnx-model'].name)
-          console.log("FILENAME: ", payload['onnx-model'].name, valid_names)
           optimizer_additional = ` --input_model=$dl_dir/${precision}/${payload['onnx-model'].name}`
           break;
         }
       }
       if (!valid_names) {
-        yield put({type: actionTypes.CONVERT_MODEL_FAILED, error: {"message": "Input model files must not contain '.' or '=' characters!"}});
+        yield put({type: actionTypes.CONVERT_MODEL_FAILED, error: {"message": "Input model file names must not contain '.' or '=' characters!"}});
         yield put({type: actionTypes.CHANGE_MODAL, payload: {error_modal: {open: true}}});
         return;
       }
